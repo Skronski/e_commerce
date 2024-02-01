2f3300fc86267d51875c3724b0f173fa02fd5ed2
Kamil Skroński <skronski@MacBook-Pro-Kamil.local>
2024-02-01 21:14:53 +0100

    direstories program addeed

---
+++ 00_programs/directories.py
@@ -0,0 +1,35 @@
+import os
+import glob
+import pandas as pd
+
+
+def directories_path_setup(_project_name, _root):
+    """
+    function is making directories (in case of it is not done):
+        - 00_programs
+        - 01_input
+        - 02_output
+        - 03_temporary
+    input:
+        - _project_name (string): (snake_case_convection)
+        - _root (string): path where to put the project input output etc.
+    returns:
+        - directories (dictionary: {string: string}): name of folder and path
+    """
+    _project_name_join = str(_project_name + "/")
+    directories = {
+        "00_programs": os.path.join(_root, _project_name_join, "00_programs"),
+        "01_input": os.path.join(_root, _project_name_join, "01_input"),
+        "02_output": os.path.join(_root, _project_name_join, "02_output"),
+        "03_temporary": os.path.join(_root, _project_name_join, "03_temporary"),
+    }
+
+    # Create directories if they don't exist
+    for directory in directories.values():
+        if os.path.exists(directory):
+            print(f"The {directory} exists already.")
+        elif not os.path.exists(directory):
+            print(f"The {directory} has been made.")
+            os.makedirs(directory)
+
+    return directories
